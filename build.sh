48896f170a40a282a10e8c6c1f60e1b57839e6bc
James Robinson <james@robinjam.net>
2018-02-21 22:20:27 +0000

    Properly namespace built images

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker build -t steam-runtime:scout           --build-arg UBUNTU=ubuntu:precise      --build-arg SOURCE=steamrt_scout.list      .
-docker build -t steam-runtime:scout_beta      --build-arg UBUNTU=ubuntu:precise      --build-arg SOURCE=steamrt_scout_beta.list .
-docker build -t steam-runtime-i386:scout      --build-arg UBUNTU=i386/ubuntu:precise --build-arg SOURCE=steamrt_scout.list      .
-docker build -t steam-runtime-i386:scout_beta --build-arg UBUNTU=i386/ubuntu:precise --build-arg SOURCE=steamrt_scout_beta.list .
+docker build -t robinjam/steam-runtime:scout           --build-arg UBUNTU=ubuntu:precise      --build-arg SOURCE=steamrt_scout.list      .
+docker build -t robinjam/steam-runtime:scout_beta      --build-arg UBUNTU=ubuntu:precise      --build-arg SOURCE=steamrt_scout_beta.list .
+docker build -t robinjam/steam-runtime-i386:scout      --build-arg UBUNTU=i386/ubuntu:precise --build-arg SOURCE=steamrt_scout.list      .
+docker build -t robinjam/steam-runtime-i386:scout_beta --build-arg UBUNTU=i386/ubuntu:precise --build-arg SOURCE=steamrt_scout_beta.list .
